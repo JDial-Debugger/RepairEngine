361e17697a66835b9998b5ea048467881af1c6a1
Matthew Kramer <kramer22@wisc.edu>
2021-08-24 21:40:22 -0500

    replace syntactic ast unit tests with integration tests

--- src/test/java/solver/script/constraint/SyntacticConstraintsASTTests.java
+++ src/test/java/solver/script/constraint/SyntacticConstraintsASTTests.java
@@ -1,114 +1,114 @@
 package solver.script.constraint;
 
-import ast.interfaces.*;
+import ast.interfaces.Expression;
+import ast.interfaces.NodeBuilder;
+import ast.interfaces.Statement;
+import ast.interfaces.Type;
+import ast.psi.NodeFactory;
+import ast.psi.NodeFactoryImpl;
+import ast.psi.PsiElementExtractorImpl;
+import ast.psi.PsiNodeBuilder;
+import ast.psi.factory.ArrayStringBuilderImpl;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.impl.PsiElementFactoryImpl;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import solver.script.solvable_modification.SolvableCodeModificationAST;
+import org.junit.jupiter.api.TestInstance;
+import solver.script.solvable_modification.SolvableCodeModification;
 import solver.script.solvable_modification.SolvableModificationId;
+import solver.script.state_record.StateRecord;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class SyntacticConstraintsASTTests {
-	private SyntacticConstraintsASTImpl astUnderTest;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SyntacticConstraintsASTTests extends LightJavaCodeInsightFixtureTestCase {
 
-	private NodeBuilder mockNodeBuilder;
-	private SolvableCodeModificationAST mockSolvableCodeModificationAST;
+	private NodeBuilder nodeBuilder;
+	private NodeFactory nodeFactory;
+	private SyntacticConstraintsAST astUnderTest;
 
-	private Expression sampleReferenceExpression;
-
-	private final static String EXPECTED_ID = "__JDIAL__syntactic_distance";
+	@BeforeAll
+	public void setup() throws Exception{
+		setUp();
+		this.myFixture.configureByText("hello.java", "public class Main { public static void main(String[] args) {System.out.println(\"hey\");}}");
+	}
 
 	@BeforeEach
-	public void setUp() {
-		this.mockNodeBuilder = mock(NodeBuilder.class);
-		this.sampleReferenceExpression = mock(Expression.class);
-		this.mockSolvableCodeModificationAST = mock(SolvableCodeModificationAST.class);
-		this.astUnderTest = new SyntacticConstraintsASTImpl(
-				this.mockNodeBuilder,
-				this.mockSolvableCodeModificationAST);
+	public void init() {
+		this.nodeFactory = new NodeFactoryImpl(getProject());
+		this.nodeBuilder = new PsiNodeBuilder(
+				new PsiElementFactoryImpl(getProject()),
+				new PsiElementExtractorImpl(),
+				new ArrayStringBuilderImpl(),
+				this.nodeFactory);
+		this.astUnderTest = new SyntacticConstraintsASTImpl(this.nodeBuilder, null);
 	}
 
 	@Test
-	public void testGetConstraintReferenceExpression() {
-		when(this.mockNodeBuilder.buildExpressionFromText(eq(EXPECTED_ID)))
-				.thenReturn(this.sampleReferenceExpression);
-		Expression actualResult = this.astUnderTest.getConstraintReferenceExpression();
-		assertEquals(this.sampleReferenceExpression, actualResult, "Did not return builder result");
-	}
+	public void testGetStateRecordChangedConstraint() {
+		WriteCommandAction.writeCommandAction(getProject()).run(() -> {
 
-	@Test
-	public void testGetInitializationStatementsOnNoIds() {
-		DeclarationStatement expectedStatement = this.stubDeclarationStatementCreation();
-		List<Statement> actualInitializationStatements
-				= this.astUnderTest.getInitializationStatements(new ArrayList<>());
-		assertThat(actualInitializationStatements, hasSize(1));
-		assertEquals(expectedStatement, actualInitializationStatements.get(0));
-	}
+			Type type = this.nodeFactory.getType(PsiType.INT);
 
-	private DeclarationStatement stubDeclarationStatementCreation() {
-		Type mockType = mock(Type.class);
-		when(this.mockNodeBuilder.buildType(PrimitiveType.INT))
-				.thenReturn(mockType);
-		LiteralExpression mockInitializer = mock(LiteralExpression.class);
-		when(this.mockNodeBuilder.buildLiteralIntExpression(0))
-				.thenReturn(mockInitializer);
-		DeclarationStatement expectedDeclaration = mock(DeclarationStatement.class);
-		when(this.mockNodeBuilder.buildDeclarationStatement(EXPECTED_ID, mockType, mockInitializer))
-				.thenReturn(expectedDeclaration);
-		return expectedDeclaration;
+			Expression actualExpr = astUnderTest.getConstraintReferenceExpression();
+
+			String expectedResult = "__JDIAL__syntactic_distance";
+			assertEquals(expectedResult, actualExpr.toString());
+		});
 	}
 
 	@Test
 	public void testGetInitializationStatements() {
-		when(this.mockNodeBuilder.buildExpressionFromText(eq(EXPECTED_ID)))
-				.thenReturn(this.sampleReferenceExpression);
+		WriteCommandAction.writeCommandAction(getProject()).run(() -> {
 
-		List<Statement> expectedStatements = new ArrayList<>();
-		expectedStatements.add(this.stubDeclarationStatementCreation());
+			Type type = this.nodeFactory.getType(PsiType.INT);
 
-		List<SolvableModificationId> inputIds = new ArrayList<>();
-		List<Statement> expectedAddStatements = this.getAndStubExpectedStatements(inputIds);
+			List<SolvableModificationId> inputIds = Arrays.asList(
+					new SolvableModificationId("func1", "var1"),
+					new SolvableModificationId("func2", "var2"),
+					new SolvableModificationId("func3", "var3"));
+			List<Statement> actualInitializationStatements
+					= this.astUnderTest.getInitializationStatements(inputIds);
 
-		expectedStatements.addAll(expectedAddStatements);
+			List<String> expectedStatements = Arrays.asList(
+					"int __JDIAL__syntactic_distance = 0;",
+					"__JDIAL__syntactic_distance+=var1;",
+					"__JDIAL__syntactic_distance+=var2;",
+					"__JDIAL__syntactic_distance+=var3;"
+			);
 
-		List<Statement> actualInitializationStatements
-				= this.astUnderTest.getInitializationStatements(inputIds);
+			List<String> fsd = actualInitializationStatements.stream().map(Object::toString).collect(
+					Collectors.toList());
 
-		assertEquals(expectedStatements, actualInitializationStatements);
+			assertThat(fsd, is(expectedStatements));
+		});
 	}
 
-	private List<Statement> getAndStubExpectedStatements(List<SolvableModificationId> inputIds) {
-		List<Statement> expectedStatements = new ArrayList<>();
-		for (int i = 0; i < 3; ++i) {
-			String changeVarName = "changeVar" + i;
-			inputIds.add(new SolvableModificationId("method" + i, changeVarName));
-			expectedStatements.add(stubExpectedStatementBuild(changeVarName));
-		}
-		return expectedStatements;
-	}
+	@Test
+	public void testGetInitializationStatementsOnNoIds() {
+		WriteCommandAction.writeCommandAction(getProject()).run(() -> {
 
-	private Statement stubExpectedStatementBuild(String changeVarName) {
-		Expression mockChangeVar = mock(Expression.class);
-		when(this.mockNodeBuilder.buildExpressionFromText(changeVarName))
-				.thenReturn(mockChangeVar);
+			Type type = this.nodeFactory.getType(PsiType.INT);
 
-		AssignExpression mockAddChange = mock(AssignExpression.class);
-		when(this.mockNodeBuilder.buildAssignExpression(this.sampleReferenceExpression,
-				AssignOperator.ADD, mockChangeVar))
-				.thenReturn(mockAddChange);
+			List<Statement> actualInitializationStatements
+					= this.astUnderTest.getInitializationStatements(new ArrayList<>());
 
-		ExpressionStatement mockAddChangeStmt = mock(ExpressionStatement.class);
-		when(this.mockNodeBuilder.buildExpressionStatement(mockAddChange))
-				.thenReturn(mockAddChangeStmt);
-
-		return mockAddChangeStmt;
+			String expectedStatement = "int __JDIAL__syntactic_distance = 0;";
+			assertEquals(expectedStatement, actualInitializationStatements.get(0).toString());
+		});
 	}
 }
